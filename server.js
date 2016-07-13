57f747b3ac586abe19cd3ad3c9f321cbbbfe6f85
David Konsumer <konsumer@jetboystudio.com>
2016-07-13 16:50:39 -0700

    not using base

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const app = express()
-const port = process.env.PORT || 5000
-
-app.use(express.static('pub'))
-
-console.log(`Listening on http://localhost:${port}`)
-
-app.listen(port)
